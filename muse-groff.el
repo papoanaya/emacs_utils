1b3d4459a53891d73eb9392282a29cd8c723853e
Luis R. Anaya <papoanaya@gmail.com>
2012-07-13 01:33:03 +0000

    Added Muse Scripts

--- muse-groff.el
+++ muse-groff.el
@@ -1,10 +1,10 @@
-;;; muse-groff.el --- publish groff -mm files
+;;; muse-groff.el --- publish groff -mom -mwww files
 
-;; Copyright (C) 2005, 2006, 2007, 2008, 2009, 2010, 2012
+;; Copyright (C) 2005, 2006, 2007, 2008, 2009, 2010
 ;;   Free Software Foundation, Inc.
 
-;; Author: Luis Roberto Anaya
-;; Date: Sun 24-Jul-2012
+;; Author: Andrew J. Korty (ajk AT iu DOT edu)
+;; Date: Tue 5-Jul-2005
 
 ;; This file is part of Emacs Muse.  It is not part of GNU Emacs.
 
@@ -37,41 +37,47 @@
 
 (require 'muse-publish)
 
-(defgroup muse-mm nil
-  "Rules for marking up a Muse file with groff -mm macros."
+(defgroup muse-groff nil
+  "Rules for marking up a Muse file with groff -mom -mwww macros."
   :group 'muse-publish)
 
-(defcustom muse-mm-extension ".mm"
-  "Default file extension for publishing mm -mom -mwww files."
+(defcustom muse-groff-extension ".groff"
+  "Default file extension for publishing groff -mom -mwww files."
   :type 'string
-  :group 'muse-mm)
+  :group 'muse-groff)
 
-(defcustom muse-mm-pdf-extension ".pdf"
-  "Default file extension for publishing mm -mm files to PDF."
+(defcustom muse-groff-pdf-extension ".pdf"
+  "Default file extension for publishing groff -mom -mwww files to PDF."
   :type 'string
-  :group 'muse-mm)
+  :group 'muse-groff)
 
-(defcustom muse-mm-header
-  ".AF \" \" 
-.TL\n<lisp>(muse-publishing-directive \"title\")</lisp>
-.AU \"<lisp>(muse-publishing-directive \"author\")</lisp>\"
-.ND \"<lisp>(muse-publishing-directive \"date\")</lisp>\"
-.MT 4\n"
-  "Header used for publishing mm -mm files."
+(defcustom muse-groff-header
+  ".TITLE \"<lisp>(muse-publishing-directive \"title\")</lisp>\"
+.SUBTITLE \"<lisp>(muse-publishing-directive \"date\")</lisp>\"
+.AUTHOR \"<lisp>(muse-publishing-directive \"author\")</lisp>\"
+.PRINTSTYLE TYPESET
+.de list
+.    LIST \\$1
+.    SHIFT_LIST \\$2
+..
+.PARA_INDENT 0
+.START
+<lisp>(and muse-publish-generate-contents \".TOC\n\")</lisp>\n"
+  "Header used for publishing groff -mom -mwww files."
   :type '(choice string file)
-  :group 'muse-mm)
+  :group 'muse-groff)
 
-(defcustom muse-mm-footer ".TC\n"
-  "Footer used for publishing groff -mm files."
+(defcustom muse-groff-footer " "
+  "Footer used for publishing groff -mom -mwww files."
   :type '(choice string file)
-  :group 'muse-mm)
+  :group 'muse-groff)
 
-(defcustom muse-mm-markup-regexps
+(defcustom muse-groff-markup-regexps
   `((10400 ,(concat "\\(\n</\\(blockquote\\|center\\)>\\)?\n"
                     "\\(["
                     muse-regexp-blank
                     "]*\n\\)+\\(<\\(blockquote\\|center\\)>\n\\)?")
-           0 muse-mm-markup-paragraph))
+           0 muse-groff-markup-paragraph))
 "List of markup regexps for identifying regions in a Muse page.
 For more on the structure of this list, see `muse-publish-markup-regexps'."
   :type '(repeat (choice
@@ -81,27 +87,27 @@ For more on the structure of this list, see `muse-publish-markup-regexps'."
                         integer
                         (choice string function symbol))
                   function))
-  :group 'muse-mm)
+  :group 'muse-groff)
 
-(defcustom muse-mm-markup-functions
-  '((table . muse-mm-markup-table))
+(defcustom muse-groff-markup-functions
+  '((table . muse-groff-markup-table))
   "An alist of style types to custom functions for that kind of text.
 For more on the structure of this list, see
 `muse-publish-markup-functions'."
   :type '(alist :key-type symbol :value-type function)
-  :group 'muse-mm)
+  :group 'muse-groff)
 
-(defcustom muse-mm-markup-tags
+(defcustom muse-groff-markup-tags
   '()
-  "A list of tag specifications, for specially marking up MM."
+  "A list of tag specifications, for specially marking up GROFF."
   :type '(repeat (list (string :tag "Markup tag")
                        (boolean :tag "Expect closing tag" :value t)
                        (boolean :tag "Parse attributes" :value nil)
                        (boolean :tag "Nestable" :value nil)
                        function))
-  :group 'muse-mm)
+  :group 'muse-groff)
 
-(defcustom muse-mm-markup-strings
+(defcustom muse-groff-markup-strings
   `((image-with-desc . "\n.MPIMG -R %s.%s\n")
     (image           . "\n.MPIMG -R %s.%s\n")
     (image-link      . "\n.\\\" %s\n.MPIMG -R %s.%s")
@@ -118,19 +124,19 @@ For more on the structure of this list, see
 ;;     (part-end        . "}")
 ;;     (chapter         . "\\chapter{")
 ;;     (chapter-end     . "}")
-    (section         . ".H 1 \"")
+    (section         . ".HEAD \"")
     (section-end     . "\"")
-    (subsection      . ".H 2 \"")
+    (subsection      . ".SUBHEAD \"")
     (subsection-end  . "\"")
-    (subsubsection   . ".H 3 \"")
+    (subsubsection   . ".PARAHEAD \"")
     (subsubsection-end . "\"")
-     (footnote        . "\n.FS\n")
-     (footnote-end    . "\n.FE\n")
+;;     (footnote        . "\\c\n.FOOTNOTE\n")
+;;     (footnote-end    . "\n.FOOTNOTE OFF\n")
 ;;     (footnotemark    . "\\footnotemark[%d]")
 ;;     (footnotetext    . "\\footnotetext[%d]{")
 ;;     (footnotetext-end . "}")
-    (begin-underline . "\n.I \"")
-    (end-underline   . "\"\n.R")
+    (begin-underline . "\n.UNDERSCORE \"")
+    (end-underline   . "\"\n")
     (begin-literal   . "\\fC")
     (end-literal     . "\\fP")
     (begin-emph      . "\\fI")
@@ -139,57 +145,59 @@ For more on the structure of this list, see
     (end-more-emph   . "\\fP")
     (begin-most-emph . "\\f(BI")
     (end-most-emph   . "\\fP")
-    (begin-verse     . "")
-    (end-verse       . "")
-    (begin-center    . "\n.DS C\n")
-    (end-center      . "\n.DE\n")
-    (begin-example   . "\n.in +5\n.I\n")
-    (end-example     . "\n.in -5\n.R\n")
-    (begin-quote     . "\n.in +5\n.I\n")
-    (end-quote       . "\n.in -5\n.R\n")
+    (begin-verse     . ".QUOTE")
+    (end-verse       . ".QUOTE OFF")
+    (begin-center    . "\n.CENTER\n")
+    (end-center      . "\n.QUAD L\n")
+    (begin-example   . ,(concat
+                         ".QUOTE_FONT CR\n.QUOTE_INDENT 1\n"".QUOTE_SIZE -2\n"
+                         ".UNDERLINE_QUOTES OFF\n.QUOTE"))
+    (end-example     . ".QUOTE OFF")
+    (begin-quote     . ".BLOCKQUOTE")
+    (end-quote       . ".BLOCKQUOTE OFF")
     (begin-cite     . "")
     (begin-cite-author . "")
     (begin-cite-year . "")
     (end-cite        . "")
-    (begin-uli       . ".BL\n")
-    (end-uli         . "\n.LE")
-    (begin-uli-item  . ".LI\n")
-    (begin-oli-item  . ".LI\n")
-    (begin-oli       . ".AL\n")
-    (end-oli         . "\n.LE")
+    (begin-uli       . ".list BULLET\n.SHIFT_LIST 2m\n")
+    (begin-uli-item . ".ITEM\n")
+    (end-uli         . "\n.LIST OFF")
+    (begin-oli       . ".list DIGIT\n.SHIFT_LIST 2m\n")
+    (begin-oli-item . ".ITEM\n")
+    (end-oli         . "\n.LIST OFF")
     (begin-ddt       . "\\fB")
     (begin-dde       . "\\fP\n.IR 4P\n")
-    (end-ddt         . "\\fR"))
+    (end-ddt         . ".IRX CLEAR"))
   "Strings used for marking up text.
 These cover the most basic kinds of markup, the handling of which
 differs little between the various styles."
   :type '(alist :key-type symbol :value-type string)
-  :group 'muse-mm)
+  :group 'muse-groff)
 
-(defcustom muse-mm-markup-specials
+(defcustom muse-groff-markup-specials
   '((?\\ . "\\e"))
   "A table of characters which must be represented specially."
   :type '(alist :key-type character :value-type string)
-  :group 'muse-mm)
+  :group 'muse-groff)
 
-(defun muse-mm-markup-paragraph ()
+(defun muse-groff-markup-paragraph ()
   (let ((end (copy-marker (match-end 0) t)))
     (goto-char (1+ (match-beginning 0)))
     (delete-region (point) end)
-    (unless (looking-at "\.\\(\\(\\(H\\|PARA\\)?HEAD \\)\\|RULE$\\)")
-      (muse-insert-markup ".P\n"))))
+    (unless (looking-at "\.\\(\\(\\(SUB\\|PARA\\)?HEAD \\)\\|RULE$\\)")
+      (muse-insert-markup ".ALD .5v\n.PP\n.ne 2\n"))))
 
-(defun muse-mm-protect-leading-chars ()
+(defun muse-groff-protect-leading-chars ()
   "Protect leading periods and apostrophes from being interpreted as
 command characters."
   (while (re-search-forward "^[.']" nil t)
     (replace-match "\\\\&\\&" t)))
 
-(defun muse-mm-concat-lists ()
+(defun muse-groff-concat-lists ()
   "Join like lists."
   (let ((type "")
         arg begin)
-    (while (re-search-forward "^\.LI[ \t]+\\(.*\\)\n" nil t)
+    (while (re-search-forward "^\.LIST[ \t]+\\(.*\\)\n" nil t)
       (setq arg (match-string 1))
       (if (string= arg "OFF")
           (setq begin (match-beginning 0))
@@ -198,7 +206,7 @@ command characters."
           (setq type arg
                 begin 0))))))
 
-(defun muse-mm-fixup-dquotes ()
+(defun muse-groff-fixup-dquotes ()
   "Fixup double quotes."
   (let ((open t))
     (while (search-forward "\"" nil t)
@@ -212,18 +220,18 @@ command characters."
           (replace-match "''")
           (setq open t))))))
 
-(defun muse-mm-prepare-buffer ()
+(defun muse-groff-prepare-buffer ()
   (goto-char (point-min))
-  (muse-mm-protect-leading-chars))
+  (muse-groff-protect-leading-chars))
 
-(defun muse-mm-munge-buffer ()
+(defun muse-groff-munge-buffer ()
   (goto-char (point-min))
-  (muse-mm-concat-lists))
+  (muse-groff-concat-lists))
 
-(defun muse-mm-pdf-browse-file (file)
+(defun muse-groff-pdf-browse-file (file)
   (shell-command (concat "open " file)))
 
-(defun muse-mm-pdf-generate (file output-path final-target)
+(defun muse-groff-pdf-generate (file output-path final-target)
   (muse-publish-transform-output
    file output-path final-target "PDF"
    (function
@@ -231,37 +239,37 @@ command characters."
       (let ((command
              (format
               (concat "file=%s; ext=%s; cd %s && cp $file$ext $file.ref && "
-                      "groff -mm -t $file$ext > $file.ps && "
+                      "groff -mom -mwww -t $file$ext > $file.ps && "
                       "ps2pdf $file.ps")
               (file-name-sans-extension file)
-              muse-mm-extension
+              muse-groff-extension
               (file-name-directory output-path))))
         (shell-command command))))
    ".ps"))
 
-;;; Register the Muse MM Publisher
+;;; Register the Muse GROFF Publisher
 
-(muse-define-style "mm"
-                   :suffix    'muse-mm-extension
-                   :regexps   'muse-mm-markup-regexps
-;;;		   :functions 'muse-mm-markup-functions
-                   :strings   'muse-mm-markup-strings
-                   :tags      'muse-mm-markup-tags
-                   :specials  'muse-mm-markup-specials
-                   :before    'muse-mm-prepare-buffer
-                   :before-end 'muse-mm-munge-buffer
-                   :header    'muse-mm-header
-                   :footer    'muse-mm-footer
+(muse-define-style "groff"
+                   :suffix    'muse-groff-extension
+                   :regexps   'muse-groff-markup-regexps
+;;;		   :functions 'muse-groff-markup-functions
+                   :strings   'muse-groff-markup-strings
+                   :tags      'muse-groff-markup-tags
+                   :specials  'muse-groff-markup-specials
+                   :before    'muse-groff-prepare-buffer
+                   :before-end 'muse-groff-munge-buffer
+                   :header    'muse-groff-header
+                   :footer    'muse-groff-footer
                    :browser   'find-file)
 
-(muse-derive-style "mm-pdf" "mm"
-                   :final   'muse-mm-pdf-generate
-                   :browser 'muse-mm-pdf-browse-file
-                   :osuffix 'muse-mm-pdf-extension)
+(muse-derive-style "groff-pdf" "groff"
+                   :final   'muse-groff-pdf-generate
+                   :browser 'muse-groff-pdf-browse-file
+                   :osuffix 'muse-groff-pdf-extension)
 
-(provide 'muse-mm)
+(provide 'muse-groff)
 
-;;; muse-mm.el ends here
+;;; muse-groff.el ends here
 ;;
 ;; Local Variables:
 ;; indent-tabs-mode: nil
