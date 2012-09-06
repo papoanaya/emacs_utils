d048e4bc7f802d3067427f5e6a21a5eedf812d5f
Luis Anaya <papoanaya@hotmail.com>
2012-09-06 19:57:28 -0400

    Fixed extra \n on .TE

--- contrib/org-e-man.el
+++ contrib/org-e-man.el
@@ -1100,7 +1100,7 @@ This function assumes TABLE has `org' as its `:type' attribute."
                                   (setq final-line (concat final-line "c" divider))))
                               final-line ))
 
-                    (format "%s\n.TE\n"
+                    (format "%s.TE\n"
                             (let ((final-line "")
                                   (long-line "")
                                   (lines (org-split-string contents "\n")))
